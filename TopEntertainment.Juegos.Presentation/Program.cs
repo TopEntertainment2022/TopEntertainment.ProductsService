4b9ac8eac2a6a12a1d7ef433c244d9d59bb91953
Rodrigo <rodrigo.persoglia@gmail.com>
2022-07-07 21:59:41 -0300

    CORS

--- TopEntertainment.Juegos.Presentation/Program.cs
+++ TopEntertainment.Juegos.Presentation/Program.cs
@@ -37,6 +37,9 @@ builder.Services.AddCors(options =>
                           policy.WithOrigins("http://127.0.0.1:5500")
                           .AllowAnyHeader()
                           .AllowAnyMethod();
+                          policy.WithOrigins("http://localhost:5500")
+.AllowAnyHeader()
+.AllowAnyMethod();
                       });
 });
 
